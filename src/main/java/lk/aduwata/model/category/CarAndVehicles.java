3da1617bde11a534c163c24b3ad98884b9d26c5f
sugeesh <lpdneo@gmail.com>
2017-08-17 00:43:51 +0530

    conflicted files resolved

--- src/main/java/lk/aduwata/model/category/CarAndVehicles.java
+++
@@ -1,7 +0,0 @@
-package lk.aduwata.model.category;
-
-/**
- * @author Sugeesh Chandraweera
- */
-public class CarAndVehicles {
-}
--- src/main/java/lk/aduwata/model/sub_category/Van.java
+++
@@ -1,7 +0,0 @@
-package lk.aduwata.model.sub_category;
-
-/**
- * @author Sugeesh Chandraweera
- */
-public class Van {
-}
